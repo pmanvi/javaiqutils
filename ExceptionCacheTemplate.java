278b7dc0d055ab84fe19bfda1fa30a1f1bffa07d
pmanvi <praveena.manvi@gmail.com>
2011-09-22 20:41:06 +0530

    Exception handling

---
+++ ExceptionCacheTemplate.java
@@ -0,0 +1,145 @@
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * This class can be used as front end to cache the exception for recurring 
+ * exceptions for heavy weight operation consuming cpu and network.
+ * It provides a container for time/resource heavy operations that result in
+ * recurring similar errors for some amount of time.
+ * Usage :
+ * 
+ * <pre>
+ *         return new ExceptionCacheTemplate<Integer,FileNotFoundException>(){
+ *              @Override
+ *              public Integer handle() throws FileNotFoundException {
+ *               // APPLICATION CODE throwing FileNotFoundExcetption
+ *                   Thread.sleep(3000); ////simulating some heavy operation
+ *                   if(true) throw new FileNotFoundException("Checked Exception");
+ *                    return 10;
+ *                  }
+ *                // APPLICATION CODE ENDS
+ *         }.runIn(new ExceptionKey("StringObject",30000));
+ * </pre>
+ */
+public abstract class ExceptionCacheTemplate<T,E extends Exception>{
+    
+    private static Map<ExceptionKey,Exception> 
+                                expCacheMap = null;
+    
+    
+    public abstract T handle() throws Exception;
+   
+    public final T runIn(final String key) throws E {
+          return runIn(new ExceptionKey(key,30000));
+    }
+    
+    public final T runIn(final ExceptionKey key) throws E{
+        try{
+           if(expCacheMap==null){
+               E e= (E)getCache().get(key);
+               if(e!=null){
+                   throw e;
+               }
+           } else {
+               // Default implementation in place
+               if(expCacheMap.containsKey(key)){
+                   if(key.isExpired()){
+                      expCacheMap.remove(key); 
+                   }else{
+                      throw expCacheMap.get(key);
+                   }
+               } 
+           }
+           return handle();
+        }catch(Exception e){
+            try{
+                E originalException =  (E) e;
+                getCache().put(key, originalException);
+                throw originalException;
+            }catch(ClassCastException cls){
+                // We got Exception other than E
+                // So if user implements the method and that translates well & good
+                // Else we will just throw back & don't cache.
+                final E translatedException = translate(e);
+                if(translatedException!=null){
+                    getCache().put(key, translatedException);
+                    throw translatedException;
+                }
+            }
+          throw new RuntimeException("UnExpected Exception "+e.toString(),e);
+        }
+    }
+    
+    /**
+     * Optional method to override,This can be used to translate the
+     * Exception to the required exception. This will make sure that
+     * if any Exception other than 'E' will also be cached.
+     * @param e
+     * @return 
+     */
+    public E translate(Exception e){
+        return null;
+    }
+    /**
+     * Optional method to override, Different Cache implementation could be
+     * provided
+     * @param e
+     * @return 
+     */
+    public Map<ExceptionKey,Exception> getCache(){
+        if(expCacheMap==null){
+            expCacheMap = new ConcurrentHashMap<ExceptionKey,Exception>();
+        }
+        return expCacheMap;
+    }
+    
+    /**
+     * A helper utility class holding the information that resulted in exception
+     * @param <K> 
+     */
+    public static class ExceptionKey<K> {
+
+        private long currentTimeStamp = 0l;
+        private K key;
+        private long expiryTime;
+        
+        public ExceptionKey(K key,long allowedTime){
+            this.key = key;
+            currentTimeStamp = System.currentTimeMillis();
+            this.expiryTime=allowedTime;
+        }
+        
+       
+        private K getKey(){
+            return key;
+        }
+        private boolean isExpired(){
+            return System.currentTimeMillis()-currentTimeStamp > this.expiryTime;
+        }
+        @Override
+        public boolean equals(final Object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final ExceptionKey other = (ExceptionKey) obj;
+            if ((this.key == null) ? (other.key != null) : !this.key.equals(other.key)) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 67 * hash + (this.key != null ? this.key.hashCode() : 0);
+            return hash;
+        }
+    }
+   
+      
+}
+
+
